b92886f03a1cccfbbd2d3e3eec0273f44d78ba48
adiapr <da801100@gmail.com>
2025-12-28 18:59:58 +0700

    add sitemap

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,25 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingpageController;
 use App\Http\Controllers\Public\ArticleController;
+use App\Models\Article;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
+
+Route::get('/sitemap', function(){
+    $sitemap = Sitemap::create()
+    ->add(Url::create('/'))
+    ->add(Url::create('/articles'));
+
+    $article = Article::get();
+    foreach($article as $item){
+        $sitemap->add(Url::create("/articles/{$item->slug}")); 
+    }
+
+    $sitemap->writeToFile(public_path('sitemap.xml'));
+
+    return redirect('/sitemap.xml');
+
+});
 
 Route::post('upload',[CKEditorController::class, 'uploadFile'])->name('upload');
 Route::get('/', [LandingpageController::class, 'index'])->name('home');
@@ -12,12 +31,6 @@ Route::get('/', [LandingpageController::class, 'index'])->name('home');
 // Public Article Routes
 Route::get('/articles', [ArticleController::class, 'index'])->name('public.articles.index');
 Route::get('/articles/{slug}', [ArticleController::class, 'show'])->name('public.articles.show');
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
 
 require __DIR__.'/auth.php';
 require __DIR__.'/admin.php';
